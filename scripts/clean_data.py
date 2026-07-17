f38072c6b3171470bc111f74b79e8e5b6fbb0950
Jack Phelan <89405607+japhelan@users.noreply.github.com>
2026-07-17 12:22:09 -0700

    Added script for implementing data cleaning notebook - todo: imputation implementation

---
+++ scripts/clean_data.py
@@ -0,0 +1,129 @@
+"""
+script for applying steps performed in data cleaning notebook to a selected dataset
+"""
+
+# setup
+import pandas as pd
+import re
+from idx import config
+import argparse
+import os
+from pathlib import Path
+
+COLS_TO_DROP = config.COLS_TO_DROP
+DT_COLS = config.DT_COLS
+INT_COLS = config.INT_COLS
+NON_PREDICTIVE_COLS = config.NON_PREDICTIVE_COLS
+NON_ANALYSIS_COLS = config.NON_ANALYSIS_COLS
+NON_NEG_FLAG_COLS = config.NON_NEG_FLAG_COLS
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Clean MLS Data")
+    parser.add_argument("input_path", type=str, help="The csv path to clean")
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        help="The output directory to save the results",
+        default=None,
+    )
+    args = parser.parse_args()
+
+    INPUT_PATH = (
+        args.input_path
+        if args.input_path
+        else os.path.join(os.path.dirname(os.path.abspath(__file__)), "input.csv")
+    )
+    OUTPUT_PATH = (
+        args.output_path
+        if args.output_path
+        else os.path.join(os.path.dirname(INPUT_PATH), "cleaned_data.csv")
+    )
+
+    print(f"Processing file {INPUT_PATH} and saving to {OUTPUT_PATH}.")
+    df = pd.read_csv(INPUT_PATH, low_memory=False)
+
+    print(f"Initial info on dataset:\n")
+    print(f"{df.shape[0]}", f"rows and {df.shape[1]} columns\n")
+
+    print("Converting proper columns to datetime and integer types...")
+    for col in DT_COLS:
+        if col in df.columns:
+            df[col] = pd.to_datetime(df[col], errors="coerce")
+    print("Datetime conversion complete.\n")
+    print("The following columns were converted to datetime:")
+    for col in DT_COLS:
+        if col in df.columns:
+            print(f" - {col}")
+
+    print("\nConverting proper columns to integer types...")
+    for col in INT_COLS:
+        if col in df.columns:
+            df[col] = df[col].astype("Int64")
+    print("Integer conversion complete.\n")
+    print("The following columns were converted to integer:")
+    for col in INT_COLS:
+        if col in df.columns:
+            print(f" - {col}")
+
+    print("\nDropping duplicate and high missing columns...")
+    actual_drops = []
+    for (
+        col
+    ) in (
+        COLS_TO_DROP
+    ):  # making sure col being dropped exists to prevent error TODO: NOT WORKING WITH SOLD DATASET
+        if col in df.columns:
+            actual_drops.append(col)
+
+    df = df.drop(columns=actual_drops, axis=1)
+    print("The following columns were dropped:")
+    for col in actual_drops:
+        print(f" - {col}")
+
+    print("\nDropping columns that are not useful for analysis...")
+    df = df.drop(columns=NON_ANALYSIS_COLS, axis=1)
+    print("The following columns were dropped:")
+    for col in NON_ANALYSIS_COLS:
+        if col in df.columns:
+            print(f" - {col}")
+
+    print("\nFlagging impossible measurement values...")
+    print("The following columns were checked for negative values:")
+    for col in NON_NEG_FLAG_COLS:
+        if col in df.columns:
+            print(f" - {col}")
+    neg_check = df[NON_NEG_FLAG_COLS] < 0
+
+    df["impossible_measurement_flag"] = neg_check.any(axis=1)
+
+    print("\nFlagging impossible dates and timeline values...")
+    print("Flagging YearBuilt for future dates...")
+    df["impossible_year_flag"] = df["YearBuilt"] > 2026
+
+    print("\nFlagging negative timeline values including...")
+    print("ListingContractDate > CloseDate")
+    print("PurchaseContractDate > CloseDate")
+    print("ListingContractDate > PurchaseContractDate")
+
+    df["listing_after_close_flag"] = df["ListingContractDate"] > df["CloseDate"]
+    df["purchase_after_close_flag"] = df["PurchaseContractDate"] > df["CloseDate"]
+    df["negative_timeline_flag"] = (
+        df["ListingContractDate"] > df["PurchaseContractDate"]
+    )
+
+    print("\nFlagging impossible, placeholder, and non-california coordinates...")
+    # lat and long checking
+    df["null_coords_flag"] = df[["Latitude", "Longitude"]].isnull().any(axis=1)
+    df["placeholder_coords_flag"] = (df["Latitude"] == 0) | (df["Longitude"] == 0)
+    df["non_cali_coords_flag"] = (
+        (df["Latitude"] > 32)
+        & (df["Latitude"] < 42)
+        & (df["Longitude"] > -124)
+        & (df["Longitude"] < -114)
+    )
+
+    print("\nCleaning complete. Final info on dataset:\n")
+    print(f"{df.shape[0]}", f"rows and {df.shape[1]} columns\n")
+    print(f"\nSaving cleaned dataset to output path: {OUTPUT_PATH}...")
+    df.to_csv(OUTPUT_PATH, index=False)
--- src/idx/config.py
+++ src/idx/config.py
@@ -16,3 +16,112 @@ NUMERIC_ANALYSIS_FIELDS = [
 ]
 DISTRIBUTION_COLS = ["ClosePrice", "LivingArea", "DaysOnMarket"]
 ENRICHED_DATA_DIR = Path("./data/enriched")
+
+
+# ================================================
+# DATA CLEANING
+# ===============================================
+
+# hopefully listing/sold agnostic but yet to be checked
+COLS_TO_DROP = [
+    "AboveGradeFinishedArea",
+    "BelowGradeFinishedArea",
+    "TaxAnnualAmount",
+    "BuilderName",
+    "ElementarySchoolDistrict",
+    "BusinessType",
+    "CoveredSpaces",
+    "MiddleOrJuniorSchool",
+    "MiddleOrJuniorSchoolDistrict",
+    "Latitude.1",
+    "Longitude.1",
+    "UnparsedAddress.1",
+    "FireplacesTotal",
+    "DaysOnMarket.1",
+    "LivingArea.1",
+    "ListPrice.1",
+    "CloseDate.1",
+    "BuyerOfficeName.1",
+    "PropertyType.1",
+    "HighSchool",
+    "ElementarySchool",
+    "ListAgentEmail",
+    "ListAgentFirstName",
+    "ListAgentLastName",
+    "ListAgentFirstName.1",
+    "ListAgentLastName.1",
+]
+
+DT_COLS = [  # cols to convert to datetime
+    "CloseDate",
+    "PurchaseContractDate",
+    "ListingContractDate",
+    "ContractStatusChangeDate",
+]
+
+INT_COLS = [  # cols to convert to integers
+    "YearBuilt",
+    "StreetNumberNumeric",
+    "BathroomsTotalInteger",
+    "TaxYear",
+    "Stories",
+]
+
+NON_PREDICTIVE_COLS = [
+    "ListingKey",
+    "CloseDate",
+    "ClosePrice",
+    "Latitude",
+    "Longitude",
+    "UnparsedAddress",
+    "ListOfficeName",
+    "BuyerOfficeName",
+    "CoListOfficeName",
+    "ListAgentFullName",
+    "CoListAgentFirstName",
+    "CoListAgentLastName",
+    "BuyerAgentMlsId",
+    "BuyerAgentFirstName",
+    "BuyerAgentLastName",
+    "ListingKeyNumeric",
+    "SubdivisionName",
+    "BuyerOfficeAOR",
+    "StreetNumberNumeric",
+    "ListingId",
+    "ContractStatusChangeDate",
+    "CoBuyerAgentFirstName",
+    "PurchaseContractDate",
+    "ListingContractDate",
+    "LotSizeDimensions",
+    "BuyerAgencyCompensationType",
+    "BuyerAgencyCompensation",
+]  # columns with minimal use in modeling
+
+NON_ANALYSIS_COLS = [
+    "ListAgentFullName",
+    "CoListAgentFirstName",
+    "CoListAgentLastName",
+    "BuyerAgentMlsId",
+    "BuyerAgentFirstName",
+    "BuyerAgentLastName",
+    "SubdivisionName",
+    "BuyerOfficeAOR",
+    "ContractStatusChangeDate",
+]  # columns not to be used in analysis (as of friday july 17th 2026)
+
+NON_NEG_FLAG_COLS = [
+    "OriginalListPrice",
+    "LivingArea",
+    "ListPrice",
+    "DaysOnMarket",
+    "ParkingTotal",
+    "LotSizeAcres",
+    "BathroomsTotalInteger",
+    "BuildingAreaTotal",
+    "BedroomsTotal",
+    "LotSizeArea",
+    "MainLevelBedrooms",
+    "GarageSpaces",
+    "LotSizeSquareFeet",
+    "ClosePrice",  # added mostly for sold data
+]
