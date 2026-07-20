2385f703ae1d0d8a8d4a18ea668dd20bac7c4ecd
Jack Phelan <89405607+japhelan@users.noreply.github.com>
2026-07-20 13:13:22 -0700

    Added fred data merging to ingest components

--- src/idx/components/ingest.py
+++ src/idx/components/ingest.py
@@ -1,5 +1,5 @@
 """
-Custom sklearn pipeline component for ingesting MLS data
+Custom sklearn pipeline component for ingesting MLS data and helper functions
 """
 
 import pandas as pd
@@ -7,6 +7,9 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from pathlib import Path
 from idx.config import DATA_DIR, RAW_LISTINGS_DIR, RAW_SOLD_DIR
 import os
+import ssl
+import urllib.request
+import certifi
 
 
 def read_mls_dir(input_dir: Path, filter: str) -> pd.DataFrame:
@@ -42,4 +45,43 @@ class MLSIngestor(BaseEstimator, TransformerMixin):
     def transform(self, X=None):
         df_sold = read_mls_dir(Path(self.input_path), "Sold")
         df_listings = read_mls_dir(Path(self.input_path), "Listing")
-        return df_sold, df_listings
+        X = df_sold, df_listings
+        return X
+
+
+class FredMerger(BaseEstimator, TransformerMixin):
+    """
+    Custom sklearn pipeline component for merging FRED data with MLS data.
+    Merges the FRED data with the MLS data on the specified date column.
+    """
+
+    def __init__(self):
+        # reading mortgage data in to be used in transform
+        self.context = ssl.create_default_context(cafile=certifi.where())
+        self.mortgage_cols = ["date", "rate_30yr_fixed"]
+        URL = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=MORTGAGE30US"
+        with urllib.request.urlopen(URL, context=self.context) as response:
+            mortgage = pd.read_csv(response, parse_dates=["observation_date"])
+        mortgage.columns = self.mortgage_cols
+        self.fred_data = mortgage
+        mortgage["year_month"] = mortgage["date"].dt.to_period("M")
+        self.mortgage_monthly = (
+            mortgage.groupby("year_month")["rate_30yr_fixed"].mean().reset_index()
+        )
+
+    def fit(self, X=None, y=None):
+        return self
+
+    def transform(self, X):
+        sold_df, listings_df = X
+        sold_df["year_month"] = pd.to_datetime(sold_df["CloseDate"]).dt.to_period("M")
+        listings_df["year_month"] = pd.to_datetime(
+            listings_df["ListingContractDate"]
+        ).dt.to_period("M")
+        sold_with_rates = sold_df.merge(
+            self.mortgage_monthly, on="year_month", how="left"
+        )
+        listings_with_rates = listings_df.merge(
+            self.mortgage_monthly, on="year_month", how="left"
+        )
+        return sold_with_rates, listings_with_rates
